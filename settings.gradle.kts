a61ab53e466d917bd263317b3d69598a2d6579f2
evitwilly <dmitry.technicov@gmail.com>
2023-12-23 19:30:16 +0700

    rewrote build scripts from Groovy into Kotlin

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,7 @@ pluginManagement {
         mavenCentral()
     }
 }
+
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
@@ -12,6 +13,14 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+
+plugins {
+    id("com.android.application") version "7.3.0" apply false
+    id("com.android.library") version "7.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
+}
+
 rootProject.name = "NiceStarRating"
-include ':app'
-include ':lib'
+
+include(":app")
+include(":lib")
